9f0cf7beb3fba1f5c6accc715397c391894066b5
Christoph Christ <office@chcnet.at>
2021-04-11 19:55:55 +0200

    anmeldung und token rücklieferung - groovy

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'tokens/create'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
         ## here we put the api endpoints
+        resources :tokens, only: [:create]
         resources :users, only: %i[show create update destroy]
     end
   end
--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,7 +2,7 @@ class JsonWebToken
   SECRET_KEY = Rails.application.credentials.secret_key_base.to_s
   
   def self.encode(payload, exp = 24.hours.from_now)
-    paylod[:exp] = exp.to_i
+    payload[:exp] = exp.to_i
     JWT.encode(payload, SECRET_KEY)
   end
   
