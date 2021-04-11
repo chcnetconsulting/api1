08e75e1e9552b139c1a5f002af513188a129ffe0
Christoph Christ <office@chcnet.at>
2021-04-11 19:29:03 +0200

    setup JWT gem

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Api1
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    ## alles aus lib laden
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
